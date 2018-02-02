5002eedf814fc7b6ac35c144c5d78ba4205ae5ac
Ron Derksen <rderksen@wehkamp.nl>
2018-02-02 11:44:10 +0100

    feature(rings): find card without diacritics

--- commands/rings.js
+++ commands/rings.js
@@ -15,9 +15,14 @@ module.exports = function rings(
     });
     return;
   }
-  logger.info(`Searching for ${name.trim()}`);
+  logger.info(`Searching for ${name}`);
   let matches = cardList
-    .filter(c => c.name.toLowerCase().indexOf(name.trim()) > -1)
+    .filter(c => c.name
+      .toLowerCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, "")
+      .indexOf(name) > -1
+    )
     .filter(c => helpers.checkFilters(c, filters));
 
   logger.info(`found ${matches.length} cards, sending response`);
@@ -36,4 +41,4 @@ module.exports = function rings(
     to: channelID,
     message
   });
-}
+};
